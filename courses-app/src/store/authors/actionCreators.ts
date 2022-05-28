129bc9a38e433a16fb2d8a108ca712181af20b4a
Inna Demidova <doc.idemidova@gmail.com>
2022-05-28 23:10:35 +0300

    added redux-thunk

--- courses-app/src/store/authors/actionCreators.ts
+++ courses-app/src/store/authors/actionCreators.ts
@@ -1,10 +1,4 @@
-import {
-	ADD_COURSES_AUTHOR,
-	CREATE_AUTHOR,
-	DELETE_COURSES_AUTHOR,
-	SET_AUTHORS,
-	SET_COURSES_AUTHORS,
-} from './actionTypes';
+import { CREATE_AUTHOR, SET_AUTHORS, SET_COURSES_AUTHORS } from './actionTypes';
 import { Author } from '../../common/models/author';
 
 export const setAuthorsAC = (authors: Author[]) => ({
@@ -14,16 +8,6 @@ export const setAuthorsAC = (authors: Author[]) => ({
 
 export const createAuthorAC = (author: Author) => ({
 	type: CREATE_AUTHOR,
-	author,
-});
-
-export const addCoursesAuthorAC = (author: Author) => ({
-	type: ADD_COURSES_AUTHOR,
-	author,
-});
-
-export const deleteCoursesAuthorAC = (author: Author) => ({
-	type: DELETE_COURSES_AUTHOR,
 	author,
 });
 
--- courses-app/src/store/courses/actionTypes.ts
+++ courses-app/src/store/courses/actionTypes.ts
@@ -1,3 +1,5 @@
 export const SET_COURSES = 'SET_COURSES';
 export const SEARCH_COURSES = 'SEARCH_COURSES';
 export const ADD_NEW_COURSE = 'ADD_NEW_COURSE';
+export const DELETE_COURSE = 'DELETE_COURSE';
+export const UPDATE_COURSE = 'UPDATE_COURSE';
